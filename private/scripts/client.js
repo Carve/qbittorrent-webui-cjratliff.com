ab798f3623454a439e80ba0bdbe923ee49a7b983
Carve <its.carve@gmail.com>
2025-08-15 09:13:38 -0400

    5.1.2 template changes

--- private/scripts/client.js
+++ private/scripts/client.js
@@ -805,10 +805,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
 
         trackerFilterList.appendChild(createLink(TRACKERS_ALL, "All (%1)", torrentsTable.getRowSize()));
-        trackerFilterList.appendChild(createLink(TRACKERS_TRACKERLESS, "Trackerless (%1)", trackerlessCount));
-        trackerFilterList.appendChild(createLink(TRACKERS_ERROR, "Tracker error (%1)", trackerErrorCount));
-        trackerFilterList.appendChild(createLink(TRACKERS_ANNOUNCE_ERROR, "Tracker announce error (%1)", announceErrorCount));
-        trackerFilterList.appendChild(createLink(TRACKERS_WARNING, "Warning (%1)", trackerWarningCount));
+        trackerFilterList.appendChild(createLink(TRACKERS_TRACKERLESS, "Trackerless", trackerlessCount));
+        trackerFilterList.appendChild(createLink(TRACKERS_ERROR, "Error", trackerErrorCount));
+        trackerFilterList.appendChild(createLink(TRACKERS_ANNOUNCE_ERROR, "Announce error", announceErrorCount));
+        trackerFilterList.appendChild(createLink(TRACKERS_WARNING, "Warning", trackerWarningCount));
 
         // Sort trackers by hostname
         const sortedList = [];
