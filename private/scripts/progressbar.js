105c3dc4b85e6535e32da1342011d4e86a629f1d
Carve <its.carve@gmail.com>
2025-10-10 11:43:24 -0400

    3.2.2 fixes

--- private/scripts/progressbar.js
+++ private/scripts/progressbar.js
@@ -109,6 +109,15 @@ window.qBittorrent.ProgressBar ??= (() => {
             this.#dark.style.clipPath = `inset(0 ${100 - this.#value}% 0 0)`;
             this.#light.style.clipPath = `inset(0 0 0 ${this.#value}%)`;
         }
+
+        invertHex(hex) {
+            return (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
+        }
+
+        setBackgroundColor(color) {
+            this.#dark.style.color = color;
+            this.#light.style.color = invertHex(color) || color;
+        }
     }
 
     customElements.define("progress-bar", ProgressBar);
